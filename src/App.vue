70db9e3df70505ffaac8ac2f020e4b14680d099e
高群2 <gaoqun@sci99.com>
2017-05-22 13:09:05 +0800

    head完成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <HeadMobile></HeadMobile>
     <router-view></router-view>
   </div>
 </template>
 
+
 <script>
+import HeadMobile from '@/components/Header';
+      
+
 export default {
-  name: 'app'
+  name: 'app',
+  components:{
+    HeadMobile
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="head">
+        <img  alt="logo" v-for="img in imgs" :class="Setclass(img.class)" :src="Setsrc(img.src)">
+        <img src="../assets/head/icon-search.png" alt="search" class="search" @click="openSearch()">
+         <div class="asideBtn">
+            <div v-for="i in 3" class="line"></div>
+        </div>
+        <div class="searchPage">
+            <input type="text" placeholder = '请输入搜索关键字'>
+            <img src="../assets/head/icon-search.png" alt="search">
+            <div class="close" @click="closeSearch()">
+                <div v-for="i in 2"></div>
+            </div>
+        </div>
+       
+    </div>
+</template>
+<script>
+    export default {
+        name: 'head',
+        data() {
+            return {
+                imgs: []
+            }
+        },
+    
+        created() {
+            this.imgs = [{
+                class: 'baijia',
+                src: require("../assets/head/logo-baijia.png")
+            },
+            {
+                class:'baidu',
+                src:require("../assets/head/logo-baidu.png")
+            }]
+        },
+        methods:{
+           Setclass(className){
+               return className;
+           },
+            Setsrc(src){
+                return src;
+            },
+            openSearch () {
+                var searchPage = document.querySelector('.searchPage')
+                searchPage.style.display = 'block'
+            },
+            closeSearch () {
+                var searchPage = document.querySelector('.searchPage')
+                searchPage.style.display = 'none'
+            }      
+        }
+    }
+</script>
